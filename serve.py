39b17ed96f978092aa3b0e83f0e0e8d5e165ef5a
Colin Oppenheim <colin.oppenheim@gmail.com>
2026-07-14 23:28:52 -0700

    Removed python scripts now that go binary is functioning as the web server

--- serve.py
+++
@@ -1,6 +0,0 @@
-from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler
-import os
-
-os.chdir('/home/pi/creality-remote-control')
-server = ThreadingHTTPServer(('0.0.0.0', 8080), SimpleHTTPRequestHandler)
-server.serve_forever()
